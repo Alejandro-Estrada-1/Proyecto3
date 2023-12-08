bb7cede5189aa7982d27ec8f63dbd3f4b6354118
Sandra <sandrarivera@ciencias.unam.mx>
2023-12-08 07:37:08 -0600

    conversion de entero a bytes con bigendian

--- src/entrada.py
+++ src/entrada.py
@@ -1,7 +1,8 @@
 import sys
-import os
-#from Crypto.Cipher import AES
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from shamir_cifrado import cifrar_archivo
+import os
+from cryptography.hazmat.backends import default_backend
 from LagrangePolinomio import obtener_llave
 
 entrada = sys.argv[1:]
@@ -26,8 +27,8 @@ if entrada[0] == 'c':
 
 
 # Descifrar
-# Leer entrada
 elif entrada[0] == 'd':
+    # Leer entrada
     if len(entrada) != 3:
         sys.exit("Error, argumentos inválidos.")
     archivo_eval = entrada[1]
@@ -42,12 +43,20 @@ elif entrada[0] == 'd':
         evaluacion = linea.split(",")
         x_valores.append(int(evaluacion[0]))
         y_valores.append(int(evaluacion[1][:-2]))
-    print("x: ", x_valores)
-    print("y: ", y_valores)
+#    print("x: ", x_valores)
+#    print("y: ", y_valores)
 
     # Descifrar archivo
-    llave = obtener_llave(x_valores, y_valores)
+    llave = int(obtener_llave(x_valores, y_valores))
     print("termino independiente: ", llave)
+    with open(archivo_cifrado, 'rb') as entrada:
+        texto_plano = entrada.read()
+    clave_secreta = llave.to_bytes(10, byteorder='little')
+    print("clave: ", clave_secreta)
+    cipher = Cipher(algorithms.AES(clave_secreta), modes.CFB(os.urandom(16)), backend=default_backend())
+    decryptor = cipher.decryptor()
+    ciphertext = decryptor.update(texto_plano) + decryptor.finalize()
+    print(ciphertext)
 
     # Crear archivo descifrado
     directorio_cifrado = os.path.dirname(archivo_cifrado)
