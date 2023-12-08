bb73d43bbd40c956fa6165fc4159abb649cf427d
Sandra <sandrarivera@ciencias.unam.mx>
2023-12-07 20:23:48 -0600

    descifrado en hasta leer evaluaciones

--- src/entrada.py
+++ src/entrada.py
@@ -1,6 +1,8 @@
 import sys
-import getpass
+import os
 #from Crypto.Cipher import AES
+from shamir_cifrado import cifrar_archivo
+from LagrangePolinomio import obtener_llave
 
 entrada = sys.argv[1:]
 if len(entrada) == 0:
@@ -20,21 +22,38 @@ if entrada[0] == 'c':
         archivo_claro = entrada[4]
     except:
         sys.exit("Error, argumentos inválidos para cifrar.")
-#    try:
-    contrasena = getpass.getpass()
-    print(contrasena)
-#    except:
-#        sys.exit("Problemas al escribir la contrasena.")
-
-
+    cifrar_archivo(archivo_claro, archivo_eval, num_eval, min_puntos)
 
 
 # Descifrar
+# Leer entrada
 elif entrada[0] == 'd':
     if len(entrada) != 3:
         sys.exit("Error, argumentos inválidos.")
     archivo_eval = entrada[1]
     archivo_cifrado = entrada[2]
+    
+    # Leer archivo con evaluaciones
+    file1 = open(archivo_eval, 'r')
+    evaluaciones = file1.readlines()
+    x_valores = []
+    y_valores = []
+    for linea in evaluaciones:
+        evaluacion = linea.split(",")
+        x_valores.append(int(evaluacion[0]))
+        y_valores.append(int(evaluacion[1][:-2]))
+    print("x: ", x_valores)
+    print("y: ", y_valores)
+
+    # Descifrar archivo
+    llave = obtener_llave(x_valores, y_valores)
+    print("termino independiente: ", llave)
+
+    # Crear archivo descifrado
+    directorio_cifrado = os.path.dirname(archivo_cifrado)
+    nombre_base, _ = os.path.splitext(archivo_cifrado)
+    archivo_claro = nombre_base + '.txt'
+
     
 else:
     sys.exit("Error, argumentos inválidos.")
