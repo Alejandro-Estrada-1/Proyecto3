a6f1cbf650c1873d27de371cafe4ef9e4b5457dd
Sandra <sandrarivera@ciencias.unam.mx>
2023-12-09 16:48:31 -0600

    maneja excepciones con archivos al descifrar

--- src/entrada.py
+++ src/entrada.py
@@ -6,13 +6,61 @@ from cryptography.hazmat.backends import default_backend
 from LagrangePolinomio import obtener_llave
 
 
-def int_to_bytes(integer):
-    if(integer < 0):
-        return integer.to_bytes(
-            length=(8 + (integer + (integer < 0)).bit_length()) // 8, byteorder='big', signed=True
+def int_to_bytes(numero):
+    if(numero < 0):
+        return numero.to_bytes(
+            (8 + (numero + (numero < 0)).bit_length()) // 8, byteorder='big', signed=True
         )
     else:
-        return integer.to_bytes((llave.bit_length() + 7) // 8, byteorder='big')
+        return numero.to_bytes(
+            (numero.bit_length() + 7) // 8, byteorder='big'
+        )
+    
+def descifrar(archivo_eval, archivo_cifrado):
+    # Leer archivo con evaluaciones
+    x_valores = []
+    y_valores = []
+    try:
+        file1 = open(archivo_eval, 'r')
+        evaluaciones = file1.readlines()
+        for linea in evaluaciones:
+            evaluacion = linea.split(",")
+            if (len(evaluacion) != 2):
+                raise Exception("Error en la linea '", linea, "'")
+            x_valores.append(int(evaluacion[0]))
+            y_valores.append(int(evaluacion[1]))
+    except Exception as error: 
+        print(error)
+        sys.exit("Ingrese un archivo valido con las evaluaciones.")
+
+
+    # Obtener la llave
+    llave = int(obtener_llave(x_valores, y_valores))
+    clave_secreta = int_to_bytes(llave)
+
+    # Descifrar archivo
+    try:
+        with open(archivo_cifrado, 'rb') as entrada:
+            texto_cifrado = entrada.read()
+        iv = os.urandom(16)
+        cipher = Cipher(algorithms.AES(clave_secreta), modes.CFB(iv), backend=default_backend())
+        decryptor = cipher.decryptor()
+        texto_descifrado = decryptor.update(texto_cifrado) + decryptor.finalize()
+    except FileNotFoundError:
+        print("No se encontro el archivo: ", archivo_cifrado)
+        sys.exit("Ingrese un archivo valido con el mensaje cifrado.")
+    except:
+        print("Ocurrio un error al leer el archivo: ", archivo_cifrado)
+        sys.exit("Ingrese un archivo valido con el mensaje cifrado.")
+
+    # Crear archivo descifrado
+    nombre_base, _ = os.path.splitext(archivo_cifrado)
+    nombre_base = "descifrado" ########### Quitar al final
+    archivo_claro = nombre_base + '.txt'
+    with open(archivo_claro, 'wb') as file:
+        file.write(texto_descifrado[16:])
+    
+    print("Archivo descifrado: ", archivo_claro)
 
 entrada = sys.argv[1:]
 if len(entrada) == 0:
@@ -42,37 +90,7 @@ elif entrada[0] == 'd':
         sys.exit("Error, argumentos inválidos.")
     archivo_eval = entrada[1]
     archivo_cifrado = entrada[2]
-    
-    # Leer archivo con evaluaciones
-    file1 = open(archivo_eval, 'r')
-    evaluaciones = file1.readlines()
-    x_valores = []
-    y_valores = []
-    for linea in evaluaciones:
-        evaluacion = linea.split(",")
-        x_valores.append(int(evaluacion[0]))
-        y_valores.append(int(evaluacion[1]))
-
-    # Obtener la llave
-    llave = int(obtener_llave(x_valores, y_valores))
-    clave_secreta = int_to_bytes(llave)
-
-    # Descifrar archivo
-    with open(archivo_cifrado, 'rb') as entrada:
-        texto_cifrado = entrada.read()
-    iv = os.urandom(16)
-    cipher = Cipher(algorithms.AES(clave_secreta), modes.CFB(iv), backend=default_backend())
-    decryptor = cipher.decryptor()
-    texto_descifrado = decryptor.update(texto_cifrado) + decryptor.finalize()
-
-    # Crear archivo descifrado
-    directorio_cifrado = os.path.dirname(archivo_cifrado)
-    nombre_base, _ = os.path.splitext(archivo_cifrado)
-    nombre_base = "descifrado" ########### Quitar al final
-    archivo_claro = nombre_base + '.txt'
-    with open(archivo_claro, 'wb') as file:
-        file.write(texto_descifrado[16:])
-
-    
+    descifrar(archivo_eval, archivo_cifrado)
+        
 else:
     sys.exit("Error, argumentos inválidos.")
